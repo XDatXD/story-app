2d067ee14ed3014f293058086e6b48f9565a3723
dangtuananh123456 <dangtuananh3012003@gmail.com>
2024-05-22 00:04:58 +0700

    5th commit

---
+++ app/(root)/classify/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Classify = () => {
+  return (
+    <div>Classify</div>
+  )
+}
+
+export default Classify
---
+++ app/(story)/story/page.tsx
@@ -0,0 +1,12 @@
+import StoryDetail from "./component/StoryDetail"
+
+
+const StoryDetailPage = () => {
+  return (
+    <div>
+      <StoryDetail/>
+    </div>
+  )
+}
+
+export default StoryDetailPage
